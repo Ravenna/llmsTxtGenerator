6d1f121649d35f79cba9325d7cdc713e02deb5c0
tjsherrill <tj@ravennainteractive.com>
2022-11-04 09:21:47 -0700

    statamic page

--- resources/js/site.js
+++ resources/js/site.js
@@ -78,3 +78,28 @@ function startStopIntroAnim(){
         header.classList.add('shown');
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    var lazyImages = [].slice.call(document.querySelectorAll("img.lazy"));
+  
+    if ("IntersectionObserver" in window) {
+      let lazyImageObserver = new IntersectionObserver(function(entries, observer) {
+        entries.forEach(function(entry) {
+          if (entry.isIntersecting) {
+            let lazyImage = entry.target;
+            console.log(lazyImage);
+            lazyImage.src = lazyImage.dataset.src;
+            lazyImage.srcset = lazyImage.dataset.srcset;
+            lazyImage.classList.remove("lazy");
+            lazyImageObserver.unobserve(lazyImage);
+          }
+        });
+      });
+  
+      lazyImages.forEach(function(lazyImage) {
+        lazyImageObserver.observe(lazyImage);
+      });
+    } else {
+      // Possibly fall back to event handlers here
+    }
+});
